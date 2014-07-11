c70c0545be6afbcc831848dcfa0923617b4404ef
Milo Gertjejansen <milo@milogert.com>
2014-07-10 22:39:41 -0400

    Started set list fnctionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from db import Card, Sets
 from flask import Flask, render_template
 from flask.ext.mongoengine import MongoEngine
 import model
+from werkzeug.routing import BaseConverter
 
 # Create the application.
 app = Flask(__name__)
@@ -13,9 +14,27 @@ app.config.from_pyfile("config.py")
 # Create the mongo database object.
 theDb = MongoEngine(app)
 
+# Set up regular expressions in routing.
+class RegexConverter(BaseConverter):
+  def __init__(self, url_map, *items):
+    super(RegexConverter, self).__init__(url_map)
+    self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
+
+
 @app.route('/')
 def index():
   return render_template("index.html", theSets=Sets.objects)
+
+@app.route("/advanced")
+def advanced():
+  return render_template("advanced.html")
+
+# Route to set pages.
+@app.route('/<regex("[a-z0-9][a-z0-9][a-z0-9]"):theSet')
+def setList(theSet):
+  return render_template("setList.html", theSet=theSet)
 
 if __name__ == '__main__':
   app.run()
