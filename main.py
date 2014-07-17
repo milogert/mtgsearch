014c9e3fe1b7ec3174a01a6c30621fdd0028da9e
Milo Gertjejansen <milo@milogert.com>
2014-07-17 14:35:02 -0400

    Removed regex url as well as added slicing plus sorting to home page.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!exec python2
 
 from db import Card, Sets
 from flask import Flask, render_template
@@ -15,26 +16,27 @@ app.config.from_pyfile("config.py")
 theDb = MongoEngine(app)
 
 # Set up regular expressions in routing.
-class RegexConverter(BaseConverter):
-  def __init__(self, url_map, *items):
-    super(RegexConverter, self).__init__(url_map)
-    self.regex = items[0]
+#class RegexConverter(BaseConverter):
+#  def __init__(self, url_map, *items):
+#    super(RegexConverter, self).__init__(url_map)
+#    self.regex = items[0]
 
-app.url_map.converters['regex'] = RegexConverter
+#app.url_map.converters['regex'] = RegexConverter
 
 
 @app.route('/')
 def index():
-  return render_template("index.html", theSets=Sets.objects)
+  aSets = Sets.objects[0:3].order_by("-releaseDate")
+  return render_template("index.html", theSets=aSets)
 
 @app.route("/advanced")
 def advanced():
   return render_template("advanced.html")
 
 # Route to set pages.
-@app.route('/<regex("[a-z0-9][a-z0-9][a-z0-9]"):theSet')
-def setList(theSet):
-  return render_template("setList.html", theSet=theSet)
+#@app.route('/<regex("[a-z0-9][a-z0-9][a-z0-9]"):theSet')
+#def setList(theSet):
+#  return render_template("setList.html", theSet=theSet)
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host="0.0.0.0")
